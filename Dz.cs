792f5d20426ef4e2d81cc7dc586fbaceaddc25c3
Ivann-Somovv <152070354+Ivann-Somovv@users.noreply.github.com>
2023-11-26 16:25:25 +0300

    Add files via upload
    eto my 2 d/z

---
+++ Dz.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //команда для удаления обьекта
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //команда для пичати слова crickets
+        print(crickets);
+    }
+}
